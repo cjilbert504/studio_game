6f406dd245a929f0b3a1fc91a5fd0ec9e3abfb61
Collin Jilbert <cjilbert504@gmail.com>
2020-10-01 17:03:44 -0500

    Initial commit

---
+++ studio_game.rb
@@ -0,0 +1,15 @@
+first_player_name = "larry"
+first_player_health = 60
+puts "#{first_player_name.capitalize} has a health of #{first_player_health}."
+
+second_player_name = "curly"
+second_player_health = 125
+puts "#{second_player_name.capitalize} has a health of #{second_player_health}."
+
+third_player_name = "moe"
+third_player_health = 100
+puts "#{third_player_name.capitalize} has a health of #{third_player_health}."
+
+fourth_player_name = "shemp"
+fourth_player_health = 90
+puts "#{fourth_player_name.capitalize} has a health of #{fourth_player_health}."
